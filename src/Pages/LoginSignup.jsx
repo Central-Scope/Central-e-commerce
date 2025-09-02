144d812b35186329e6e9374e9009694df4cd7178
Central Scope <agent4scope@gmail.com>
2025-09-02 12:45:39 +0100

    Login-up & signup page addition

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,5 +1,29 @@
+import "./css/LoginSignup.css";
+
 const LoginSignup = () => {
-    return <div></div>;
+    return (
+        <div className="loginsignup">
+            <div className="loginsignup-container">
+                <h1>Sign Up</h1>
+                <div className="loginsignup-fields">
+                    <input type="text" placeholder="Your name" />
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                </div>
+                <button>Continue</button>
+                <p className="loginsignup-login">
+                    Already have an account? <span>Login here</span>
+                </p>
+                <div className="loginsignup-agree">
+                    <input type="checkbox" name="" id="" />
+                    <p>
+                        By continuing, I agree to the terms & conditions of use
+                        .
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default LoginSignup;
