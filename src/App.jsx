0d70f78e36442ce197e6dcb2bb1187111247b4d3
Central Scope <agent4scope@gmail.com>
2025-09-02 12:02:29 +0100

    Styling and markup of men, women and kids pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
 import Footer from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
     return (
@@ -17,15 +20,24 @@ function App() {
                     <Route path="/" element={<Shop />} />
                     <Route
                         path="/mens"
-                        element={<ShopCategory category="men" />}
+                        element={
+                            <ShopCategory banner={men_banner} category="men" />
+                        }
                     />
                     <Route
                         path="/womens"
-                        element={<ShopCategory category="women" />}
+                        element={
+                            <ShopCategory
+                                banner={women_banner}
+                                category="women"
+                            />
+                        }
                     />
                     <Route
                         path="/kids"
-                        element={<ShopCategory category="kid" />}
+                        element={
+                            <ShopCategory banner={kid_banner} category="kid" />
+                        }
                     />
                     <Route path="/product" element={<Product />}>
                         <Route path=":productId" element={<Product />} />
